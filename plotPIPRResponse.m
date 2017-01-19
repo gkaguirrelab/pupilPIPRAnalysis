2eaf7f6275947b6cca8e6a23e360579b2c76163a
harrison-mcadams <harrison.mcadams@gmail.com>
2017-01-18 19:09:30 -0500

    removed application of exclusion criteria to a separate function
    to apply exclusion criteria, now call excludeSubjects
    -now requires a subject list for input

--- plotPIPRResponse.m
+++ plotPIPRResponse.m
@@ -1,4 +1,4 @@
-function [theResult] = plotPIPRResponse()
+function [piprCombined, averageMelCombined, averageLMSCombined, averageBlueCombined, averageRedCombined] = plotPIPRResponse(goodSubjects, dropboxAnalysisDir)
 
 % Function to plot the average PIPR response
 % First, we determine which subjects can be kept for the analysis because
@@ -9,80 +9,6 @@ function [theResult] = plotPIPRResponse()
 
 % 12/12/2016, written by hmm
 
-
-%% Setup basic variables
-% Discover user name and set Dropbox path
-[~, userName] = system('whoami');
-userName = strtrim(userName);
-dropboxAnalysisDir = ...
-    fullfile('/Users', userName, ...
-    '/Dropbox (Aguirre-Brainard Lab)/MELA_analysis/');
-subAnalysisDirectory = 'PIPRMaxPulse_PulsePIPR/PIPRAverageResponse';
-
-% Obtain list of subjects. Dynanmically figures out who the subjects are
-% based on the contents of the PIPRMaxPulse_PulsePIPR folder
-subjectList = [];
-dirSubjectList = dir(fullfile(dropboxAnalysisDir, 'PIPRMaxPulse_PulsePIPR'));
-% remove first two entries, which are "." and ".."
-dirSubjectList = dirSubjectList(3:length(dirSubjectList));
-for ss = 1:length(dirSubjectList);
-    name = dirSubjectList(ss).name;
-    % only include MELA subjects, not TEST subjects
-    firstFour = dirSubjectList(ss).name(1:4);
-    if strcmp(firstFour, 'MELA');
-        subjectList = [subjectList; name];
-    end
-end
-
-%% First, determine which subjects had data of high enough quality to avoid exclusion criteria
-% For each subject, we're going to read in the subject's corresponding
-% DataQuality.csv file which contains information about number of accepted
-% vs. rejected trials
-% Exclusion criteria, from the pre-registration document:
-% ?	If, over all trials (PIPR, Mel, LMS), more than 50% of trials are identified as ?incomplete?
-% ?	If, within a given trial block (PIPR, Mel, or LMS) more than 75% of trials are identified as ?incomplete?.
-
-blockTypes = {'PIPR', 'Mel', 'LMS'};
-goodSubjects = [];
-badSubjects = [];
-
-for ss = 1:length(subjectList);
-    subject = subjectList(ss,:);
-    % if failurePotential =/= 0, that means this subject meets exclusion
-    % criteria and will be discarded
-    failurePotential = 0;
-    totalFailedTrials = 0;
-    totalTrials = 0;
-    for bb = 1:length(blockTypes)
-        blockFailedTrials = 0;
-        blockTotalTrials = 0;
-        dataQualityCSV = importdata(fullfile(dropboxAnalysisDir, ['PIPRMaxPulse_Pulse', blockTypes{bb}], subject, [subject, '_PupilPulseData_DataQuality.csv']));
-        trialTypes = size(dataQualityCSV.data,1)-1;
-        for tt = 1:trialTypes;
-            % keep track of total number of trials
-            totalTrials = totalTrials + dataQualityCSV.data(tt,2);
-            % keep track of total number of trials within a given block
-            blockTotalTrials = blockTotalTrials + dataQualityCSV.data(tt,2);
-            % keep track of total number of failed trials
-            totalFailedTrials = totalFailedTrials + dataQualityCSV.data(tt,1);
-            % keep track of total number of failed trials within a given
-            % block
-            blockFailedTrials = blockFailedTrials + dataQualityCSV.data(tt,1);
-        end
-        if blockFailedTrials/blockTotalTrials > 0.75;
-            failurePotential = failurePotential + 1;
-        end
-    end
-    if totalFailedTrials/totalTrials > 0.50;
-        failurePotential = failurePotential +1;
-    end
-    if failurePotential == 0;
-        goodSubjects = [goodSubjects; subject];
-    end
-    if failurePotential ~= 0;
-        badSubjects = [badSubjects; subject];
-    end
-end
 
 
 %% Create plots across subjects that show the average response to the red
@@ -117,6 +43,8 @@ for ss = 1:length(goodSubjects);
         semPipr(1,timepoints) = (semBlue(1,timepoints)^2 + semRed(1,timepoints)^2)^(1/2);
     end
     piprCombined(ss,:) = averageBlue-averageRed;
+    averageBlueCombined(ss,:) = averageBlue;
+    averageRedCombined(ss,:) = averageRed;
     % now do the plotting per subject
     plotFig = figure;
     errBar(1,:) = semBlue(1:(length(averageBlue)));
@@ -137,7 +65,7 @@ for ss = 1:length(goodSubjects);
     xlabel('Time (s)');
     ylabel('Percent Change (%)');
     ylim([-60 20]);
-    outDir = fullfile(dropboxAnalysisDir,subAnalysisDirectory);
+    outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulsePIPR/AverageResponse');
     if ~exist(outDir, 'dir')
         mkdir(outDir);
     end
@@ -175,7 +103,7 @@ shadedErrorBar((1:length(piprCollapsed))*0.02,piprCollapsed*-100, errBar*100, 'k
 xlabel('Time (s)');
 ylabel('Percent Change (%)');
 ylim([-60 20]);
-outDir = fullfile(dropboxAnalysisDir,subAnalysisDirectory);
+    outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulsePIPR/AverageResponse');
 if ~exist(outDir, 'dir')
     mkdir(outDir);
 end
