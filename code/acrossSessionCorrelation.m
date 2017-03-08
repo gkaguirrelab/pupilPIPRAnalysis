01514864e958a01d92989c13ceb5a283fb011d57
harrison-mcadams <harrison.mcadams@gmail.com>
2017-03-08 18:11:14 -0500

    now adds spearman correlation to plots

--- code/acrossSessionCorrelation.m
+++ code/acrossSessionCorrelation.m
@@ -42,6 +42,11 @@ for ss = 1:size(subjects{2},1) % loop over subjects that have completed both ses
     bluePlusRedTwo(ss) = amplitudes{2}(secondSessionIndex,9)*100;
     semBluePlusRedOne(ss) = amplitudesSEM{1}(firstSessionIndex,9)*100;
     semBluePlusRedTwo(ss) = amplitudesSEM{2}(secondSessionIndex,9)*100;
+    
+    blueToRedOne(ss) = amplitudes{1}(firstSessionIndex,7);
+    blueToRedTwo(ss) = amplitudes{2}(secondSessionIndex,7);
+    semBlueToRedOne(ss) = amplitudesSEM{1}(firstSessionIndex,7);
+    semBlueToRedTwo(ss) = amplitudesSEM{2}(secondSessionIndex,7);
 end
 
 % now do some plotting
@@ -62,14 +67,39 @@ axis square
 maxValue = max(max(melNormedOne,melNormedTwo));
 xlim([ 0 maxValue ]);
 ylim([ 0 maxValue ]);
+% determine spearman correlation:
+rho = corr(melNormedOne', melNormedTwo', 'type', 'Spearman');
+legend(['rho = ', num2str(rho)])
+
 saveas(plotFig, fullfile(outDir, ['melNormedTestRetest.png']), 'png');
 close(plotFig);
+
+% plot blue response normed by response to red (that is blue/red amplitude
+% ratio)
+plotFig = figure;
+hold on
+errorbar(blueToRedOne, blueToRedTwo, semBlueToRedTwo, 'bo')
+herrorbar(blueToRedOne, blueToRedTwo, semBlueToRedOne, 'bo')
+plot(0:10,0:10,'-')
+xlabel('Blue/Red Session 1')
+ylabel('Blue/Red Session 2')
+axis square
+maxValue = max(max(blueToRedOne,blueToRedTwo));
+xlim([ 0 maxValue ]);
+ylim([ 0 maxValue ]);
+% determine spearman correlation:
+rho = corr(blueToRedOne', blueToRedTwo', 'type', 'Spearman');
+legend(['rho = ', num2str(rho)])
+saveas(plotFig, fullfile(outDir, ['blueToRedTestRetest.png']), 'png');
+close(plotFig);
+
 
 % next pipr response
 plotFig = figure;
 hold on
 errorbar(piprOne, piprTwo, semPIPRTwo, 'bo')
 herrorbar(piprOne, piprTwo, semPIPROne, 'bo')
+plot(-100:100,-100:100,'-')
 xlabel('PIPR (%) Session 1')
 ylabel('PIPR (%) Session 2')
 maxValue = max(max(piprOne,piprTwo));
@@ -77,6 +107,9 @@ minValue = min(min(piprOne,piprTwo));
 xlim([ minValue maxValue ]);
 ylim([ minValue maxValue ]);
 axis square
+% determine spearman correlation:
+rho = corr(piprOne', piprTwo', 'type', 'Spearman');
+legend(['rho = ', num2str(rho)])
 saveas(plotFig, fullfile(outDir, ['PIPRTestRetest.png']), 'png');
 close(plotFig);
 
@@ -85,12 +118,16 @@ plotFig = figure;
 hold on
 errorbar(melPlusLMSOne, melPlusLMSTwo, semMelPlusLMSTwo, 'bo')
 herrorbar(melPlusLMSOne, melPlusLMSTwo, semMelPlusLMSOne, 'bo')
+plot(-100:100,-100:100,'-')
 xlabel('(Mel+LMS)/2 (%) Session 1')
 ylabel('(Mel+LMS)/2 (%) Session 2')
 maxValue = max(max(melPlusLMSOne,melPlusLMSTwo));
 minValue = min(min(melPlusLMSOne,melPlusLMSTwo));
 xlim([ minValue maxValue ]);
 ylim([ minValue maxValue ]);
+% determine spearman correlation:
+rho = corr(melPlusLMSOne', melPlusLMSTwo', 'type', 'Spearman');
+legend(['rho = ', num2str(rho)])
 axis square
 saveas(plotFig, fullfile(outDir, ['melPlusLMSTestRetest.png']), 'png');
 close(plotFig);
@@ -100,6 +137,7 @@ plotFig = figure;
 hold on
 errorbar(bluePlusRedOne, bluePlusRedTwo, semBluePlusRedTwo, 'bo')
 herrorbar(bluePlusRedOne, bluePlusRedTwo, semBluePlusRedOne, 'bo')
+plot(-100:100,-100:100,'-')
 xlabel('(Blue+Red)/2 (%) Session 1')
 ylabel('(Blue+Red)/2 (%) Session 2')
 maxValue = max(max(bluePlusRedOne,bluePlusRedTwo));
@@ -107,6 +145,9 @@ minValue = min(min(bluePlusRedOne,bluePlusRedTwo));
 xlim([ minValue maxValue ]);
 ylim([ minValue maxValue ]);
 axis square
+% determine spearman correlation:
+rho = corr(bluePlusRedOne', bluePlusRedTwo', 'type', 'Spearman');
+legend(['rho = ', num2str(rho)])
 saveas(plotFig, fullfile(outDir, ['bluePlusRedTestRetest.png']), 'png');
 close(plotFig);
 
