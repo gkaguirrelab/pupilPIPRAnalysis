62134bbaf61e5568fa834646b1234b06bc767681
harrison-mcadams <harrison.mcadams@gmail.com>
2017-05-02 13:26:45 -0400

    new function to calculate the first derivative of pupil time series data
    inspired by work by Lowenstein that dissected the parasympathetic from
    sympathetic contributions to pupil diameter

---
+++ code/calculateDerivative.m
@@ -0,0 +1,25 @@
+function [] = calculateDerivative(pupilPacket)
+
+numberTimepoints = 7;
+
+for timepoint = 1:length(pupilPacket.response.values)
+    if timepoint < round(numberTimepoints/2)
+        x = 1:(timepoint-1)+timepoint;
+        y = pupilPacket.response.values(1:length(x));
+        
+    elseif timepoint > length(pupilPacket.response.values) - (round(numberTimepoints/2)-1);
+        x = (timepoint - (length(pupilPacket.response.values) - timepoint)):length(pupilPacket.response.values);
+        y = pupilPacket.response.values(x);
+    else  
+        x = (timepoint-(round(numberTimepoints/2-1)):timepoint+(round(numberTimepoints/2-1)));
+        y = pupilPacket.response.values(x);
+    end
+        coefficients = polyfit(x,y,1);
+        slope = coefficients(1);
+        derivative(timepoint) = slope;
+end
+
+plot(pupilPacket.response.timebase, abs(derivative))
+
+
+        
