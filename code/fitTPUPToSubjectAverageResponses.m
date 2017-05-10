809766d06861c38fa81cc543cbbbae6ea7c47759
harrison-mcadams <harrisonmcadams@hntvlan554.00105.wlan.sas.upenn.edu>
2017-05-10 19:21:26 -0400

    compatible with new directory structure

--- code/fitTPUPToSubjectAverageResponses.m
+++ code/fitTPUPToSubjectAverageResponses.m
@@ -1,4 +1,4 @@
-function [ TPUPAmplitudes, temporalParameters ] = fitTPUPToSubjectAverageResponses(goodSubjects, piprCombined, averageMelCombined, averageLMSCombined, averageRedCombined, averageBlueCombined, dropboxAnalysisDir)
+function [ TPUPAmplitudes, temporalParameters, varianceExplained ] = fitTPUPToSubjectAverageResponses(goodSubjects, piprCombined, averageMelCombined, averageLMSCombined, averageRedCombined, averageBlueCombined, dropboxAnalysisDir)
 
 % The main output will be an [ss x 3] matrix, called amplitude, which contains the results
 % from fitting the IAMP model to to average responses per subject. The
@@ -78,32 +78,35 @@ for session = 1:2;
     for ss = 1:size(goodSubjects{session}{1},1) % loop over subjects
         subject = goodSubjects{session}{1}(ss,:);
         
+        subDir = 'pupilPIPRAnalysis/TPUP/modelFits';
         for stimulation = 1:length(stimulusOrder);
             if stimulation == 1; % LMS condition
-                outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulseLMS/TPUP', num2str(session));
+                outDir = fullfile(dropboxAnalysisDir, subDir, stimulusOrder{stimulation}, num2str(session));
                 %params = LMSParams;
                 result = 100*averageLMSCombined{session}(ss, :);
                 
             elseif stimulation == 2; % mel condition
                 %params = MelParams;
-                outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulseMel/TPUP', num2str(session));
+                outDir = fullfile(dropboxAnalysisDir, subDir, stimulusOrder{stimulation}, num2str(session));
                 
                 result = 100*averageMelCombined{session}(ss, :);
                 
                 
             elseif stimulation == 3; % blue condition
                 %params = blueParams;
-                outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulsePIPR/TPUP', num2str(session));
+                outDir = fullfile(dropboxAnalysisDir, subDir, stimulusOrder{stimulation}, num2str(session));
                 
                 result = 100*averageBlueCombined{session}(ss, :);
             elseif stimulation == 4; % red condition
                 %params = redParams;
-                outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulsePIPR/TPUP', num2str(session));
+                outDir = fullfile(dropboxAnalysisDir, subDir, stimulusOrder{stimulation}, num2str(session));
                 
                 result = 100*averageRedCombined{session}(ss, :);
                 
             end
-            
+            if ~exist(outDir, 'dir')
+                mkdir(outDir);
+            end
             
             
             
@@ -120,8 +123,8 @@ for session = 1:2;
             %vub=[params{1}(1), params{1}(2), params{1}(3), 0, 0, 0];
             
             initialValues=[200, 200, 10, -10, -25, -25];
-            vlb=[-500, 100, 1, -2000, -2000, -2000];
-            vub=[0, 500, 30, 0, 0, 0];
+            vlb=[-500, 150, 1, -2000, -2000, -2000];
+            vub=[0, 750, 30, 0, 0, 0];
             
             % do the actual fitting via TPUP
             [paramsFit,fVal,modelResponseStruct] = temporalFit.fitResponse(thePacket, 'defaultParamsInfo', defaultParamsInfo, 'initialValues', initialValues, 'vlb', vlb, 'vub',vub);
@@ -181,73 +184,7 @@ end
 
 
 
-%% make a bar graph to compare relative contributions of each parameter to the final fit
 
-for session = 1:2
-    amplitudes = [];
-    sem = [];
-    for stimulus = 1:length(stimulusOrder)
-        for measure = 1:3 % we have three measures of amplitude: transient, sustained, persistent as well as three temporal parameters
-            amplitudes(measure,stimulus) = mean(TPUPAmplitudes{session}{stimulus}(:,measure));
-            sem(measure,stimulus) = std(TPUPAmplitudes{session}{stimulus}(:,measure))/sqrt(length(TPUPAmplitudes{session}{stimulus}));
-            meanTemporalParameters(measure,stimulus) = mean(temporalParameters{session}{stimulus}(:,measure));
-            semTemporalParameters(measure,stimulus) = std(temporalParameters{session}{stimulus}(:,measure))/sqrt(length(temporalParameters{session}{stimulus}));
-            
-        end
-    end
-    
-    
-    plotFig = figure;
-    
-    b = barwitherr(sem, amplitudes);
-    b(1).FaceColor = 'c';
-    b(2).FaceColor = 'm';
-    b(3).FaceColor = 'b';
-    b(4).FaceColor = 'r';
-    set(gca,'XTickLabel',{'Transient', 'Sustained', 'Persistent'})
-    title('Mean TPUP Amplitudes')
-    legend('LMS', 'Mel', 'Blue', 'Red', 'Location', 'SouthWest')
-    outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulsePIPR/TPUP', num2str(session));
-    saveas(plotFig, fullfile(outDir, ['compareStimuli.png']), 'png');
-    close(plotFig);
-    
-    
-    % now compare the temporal parameters
-    % first set the delay to positive
-    for stimulus = 1:length(stimulusOrder)
-        meanTemporalParameters(1,stimulus) = meanTemporalParameters(1,stimulus) * -1;
-    end
-    
-    plotFig = figure;
-    b = barwitherr(semTemporalParameters, meanTemporalParameters);
-    b(1).FaceColor = 'c';
-    b(2).FaceColor = 'm';
-    b(3).FaceColor = 'b';
-    b(4).FaceColor = 'r';
-    set(gca,'XTickLabel',{'Delay', 'Gamma Tau', 'Exponential Tau'})
-    legend('LMS', 'Mel', 'Blue', 'Red', 'Location', 'NorthEast')
-    title('Mean Temporal Parameters')
-    outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulsePIPR/TPUP', num2str(session));
-    saveas(plotFig, fullfile(outDir, ['compareStimuli_temporalParameters.png']), 'png');
-    close(plotFig);
-    
-    % now compare the median variance explained
-    % first calculate the median variance for each stimulation
-    for stimulus = 1:length(stimulusOrder)
-        medianVarianceExplained(1,stimulus) = median(varianceExplained{session}{stimulus}(:,1));
-        iqrVarianceExplained(1,stimulus) = iqr(varianceExplained{session}{stimulus}(:,1));
-    end
-    % now do the plotting
-    plotFig = figure;
-    b = barwitherr(1/2*iqrVarianceExplained, medianVarianceExplained);
-    
-    set(gca,'XTickLabel',{'LMS', 'Mel', 'Blue', 'Red'})
-    %legend('LMS', 'Mel', 'Blue', 'Red', 'Location', 'SouthWest')
-    title('Median Variance Explained')
-    outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulsePIPR/TPUP', num2str(session));
-    saveas(plotFig, fullfile(outDir, ['compareStimuli_medianVarianceExplained.png']), 'png');
-    close(plotFig);
-end
 
 %% do some plotting to summarize the results
 % determine group averages
@@ -325,26 +262,27 @@ for session = 1:2;
     thePacket.kernel = [];
     for stimulation = 1:length(stimulusOrder)
         if stimulation == 1; % LMS condition
-            outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulseLMS/TPUP', num2str(session));
+            outDir = fullfile(dropboxAnalysisDir, subDir, stimulusOrder{stimulation}, num2str(session));
             
             result = averageLMSCollapsed{session};
             
         elseif stimulation == 2; % mel condition
-            outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulseMel/TPUP', num2str(session));
+            outDir = fullfile(dropboxAnalysisDir, subDir, stimulusOrder{stimulation}, num2str(session));
             
             result = averageMelCollapsed{session};
             
             
         elseif stimulation == 3; % blue condition
-            outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulsePIPR/TPUP', num2str(session));
+            outDir = fullfile(dropboxAnalysisDir, subDir, stimulusOrder{stimulation}, num2str(session));
             
             result = averageBlueCollapsed{session};
         elseif stimulation == 4; % red condition
-            outDir = fullfile(dropboxAnalysisDir,'PIPRMaxPulse_PulsePIPR/TPUP', num2str(session));
+            outDir = fullfile(dropboxAnalysisDir, subDir, stimulusOrder{stimulation}, num2str(session));
             
             result = averageRedCollapsed{session};
             
         end
+        
         
         thePacket.response.timebase = timebase;
         thePacket.response.values = result*100;
@@ -353,13 +291,32 @@ for session = 1:2;
         
         
         initialValues=[200, 200, 10, -10, -25, -25];
-        vlb=[-500, 100, 1, -2000, -2000, -2000];
-        vub=[0, 500, 30, 0, 0, 0];
+        vlb=[-500, 150, 1, -2000, -2000, -2000];
+        vub=[0, 750, 30, 0, 0, 0];
         
         % do the fitting on the group average data
         [paramsFit,fVal,modelResponseStruct] = temporalFit.fitResponse(thePacket, 'defaultParamsInfo', defaultParamsInfo, 'initialValues', initialValues, 'vlb', vlb, 'vub',vub); % with
         %first three parameters fixed
         %[paramsFit,fVal,modelResponseStruct] = temporalFit.fitResponse(thePacket, 'defaultParamsInfo', defaultParamsInfo,'paramLockMatrix',paramLockMatrix);
+        
+        % create a model fit for the group average that is the result of
+        % the average parameters of the fits to individual subjects
+        params = temporalFit.defaultParams;
+        params.paramMainMatrix(1) = median(temporalParameters{session}{stimulation}(:,1));
+        params.paramMainMatrix(2) = median(temporalParameters{session}{stimulation}(:,2));
+        params.paramMainMatrix(3) = median(temporalParameters{session}{stimulation}(:,3));
+        params.paramMainMatrix(4) = median(TPUPAmplitudes{session}{stimulation}(:,1));
+        params.paramMainMatrix(5) = median(TPUPAmplitudes{session}{stimulation}(:,2));
+        params.paramMainMatrix(6) = median(TPUPAmplitudes{session}{stimulation}(:,3));
+        % kernel struct will be empty for TPUP
+        kernelStruct = [];
+        
+        
+        
+        averageFit = temporalFit.computeResponse(params,stimulusStruct,kernelStruct,'AddNoise',false);
+        
+        
+        
         
         if ~exist(outDir, 'dir')
             mkdir(outDir);
@@ -373,7 +330,7 @@ for session = 1:2;
             plot(timebase/1000, thePacket.response.values)
             hold on
             plot(timebase/1000, modelResponseStruct.values)
-            plot(timebase/1000, averageLMSFit{session}(1,:))
+            plot(timebase/1000, averageFit.values)
             xlabel('Time (s)')
             ylabel('Pupil Diameter (% Change)')
             legend('Group Data', 'TPUP Fit', 'Average Fit')
@@ -403,7 +360,7 @@ for session = 1:2;
             plot(timebase/1000, thePacket.response.values)
             hold on
             plot(timebase/1000, modelResponseStruct.values)
-            plot(timebase/1000, averageMelFit{session}(1,:))
+            plot(timebase/1000, averageFit.values)
             
             xlabel('Time (s)')
             ylabel('Pupil Diameter (% Change)')
@@ -432,7 +389,7 @@ for session = 1:2;
             plot(timebase/1000, thePacket.response.values)
             hold on
             plot(timebase/1000, modelResponseStruct.values)
-            plot(timebase/1000, averageBlueFit{session}(1,:))
+            plot(timebase/1000, averageFit.values)
             legend('Group Data', 'TPUP Fit', 'Average Fit')
             
             
@@ -463,7 +420,7 @@ for session = 1:2;
             plot(timebase/1000, thePacket.response.values)
             hold on
             plot(timebase/1000, modelResponseStruct.values)
-            plot(timebase/1000, averageRedFit{session}(1,:))
+            plot(timebase/1000, averageFit.values)
             legend('Group Data', 'TPUP Fit', 'Average Fit')
             
             % determine variance explained
