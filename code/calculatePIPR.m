85f60e1f481068fe30eafb602f3e59728711176d
harrison-mcadams <harrisonmcadams@hntvlan554.00308.wlan.wireless-pennnet.upenn.edu>
2017-07-05 20:08:59 -0400

    updated function to allow for flexible computations of the PIPR
    - input parser allows us to specify the specific calculation
    - specific computation options include: "window" and "totalAmplitude"
    - "window": calculate sustained constriction over a given window of
    time; also need to specify timeOn and timeOff of the window
    - "totalAmplitude": use the IAMP approach to estimate amplitude of blue
    and red constriction, and use that subtraction to be the PIPR

--- code/calculatePIPR.m
+++ code/calculatePIPR.m
@@ -1,4 +1,14 @@
-function [ sustainedAmplitudes, pipr, netPipr ] = calculatePIPR(subjects, amplitudes, amplitudesSEM, averageBlueCombined, averageRedCombined, dropboxAnalysisDir)
+function [ pipr, netPipr ] = calculatePIPR(subjects, amplitudes, dropboxAnalysisDir, varargin)
+
+% the purpose of this function is to calculate the PIPR according to a
+% couple of different possible methods.
+
+% The first basic methodology for computing the PIPR found here is to look
+% at the sustained amplitude. Sustained refers to a period of time after
+% light-offset while the pupil is still constricted relative to baseline.
+% When the sustained window occurs can be specified in calling the
+% function, either by picking a single instant in time or picking a window
+% of time. For some references about certain sustained windows to chose:
 
 % The purpose of this function is to calculate the PIPR a list of subjects
 % by following the protocol laid out by Kankipati 2010
@@ -22,133 +32,172 @@ function [ sustainedAmplitudes, pipr, netPipr ] = calculatePIPR(subjects, amplit
 % because our stimuli come too frequently; we cannot have a 30 second
 % period beginning 10 s after stimulus offset to define our sustained
 % amplitude because the pupil will already be responding to the next
-% stimulus. We will also be thinking about the pupil in terms of percentage
-% change from baseline, so we will specifically be calculating the PIPR
-% change % and the net PIPR change %
-% For our purposes, we will define sustained pupil size as pupil diameter
-% averaged over a 1 s window (the longest we can achieve) starting
-% from 10 seconds after stimulus offset
-stimulusOnsetTime = 0;
-stimulusOffsetTime = 3;
-%sustainedOnsetTime = 10; % 10 s after stimulus offset
-sustainedOnsetTime = 4.46 % when the response to melanopsin is scaled to the same size as the response to LMS stimulation, the melanopsin response crosses over at 4.46 s
+% stimulus. 
+
+% some reasonable default parameters
+sustainedOnsetTime = 4.46; % when the response to melanopsin is scaled to the same size as the response to LMS stimulation, the melanopsin response crosses over at 4.46 s
 sustainedOffsetTime = 14; % to the end of an individual trial
+
 % we are sampling pupil diameter every 20 ms, so this time window will
 % include the following indices
 sustainedWindow = (sustainedOnsetTime/0.02):(sustainedOffsetTime/0.02);
 
+% input parsing
+p = inputParser; p.KeepUnmatched = true;
 
-%% First to calculate the sustained amplitudes
-% For each subject for each trial of red and blue light, determine the
-% average sustained amplitude. Then determine the average sustained
-% amplitude for each subject across all red or all blue trials
-for session = 1:2;
-    sustainedAmplitudes{session} = [];
-    pipr{session} = [];
-    netPipr{session} = [];
-end
+p.addParameter('computeMethod','window',@ischar);
+p.addParameter('plot','no',@ischar);
+p.addParameter('timeOn',sustainedOnsetTime,@isnumeric);
+p.addParameter('timeOff',sustainedOffsetTime,@isnumeric);
 
-for session = 1:2;
-    for ss = 1:size(subjects{session}{1},1);
-        subject = subjects{session}{1}(ss,:);
-        numberSessions = dir(fullfile(dropboxAnalysisDir, 'PIPRMaxPulse_PulsePIPR', subject));
-        numberSessions =length(numberSessions(~ismember({numberSessions.name},{'.','..', '.DS_Store'})));
-        
-        date = subjects{session}{2}(ss,:);
-        
-        blue = importdata(fullfile(dropboxAnalysisDir, 'PIPRMaxPulse_PulsePIPR', subject, date, [subject, '_PupilPulseData_PIPRBlue_TimeSeries.csv']));
-        red = importdata(fullfile(dropboxAnalysisDir, 'PIPRMaxPulse_PulsePIPR', subject, date, [subject, '_PupilPulseData_PIPRRed_TimeSeries.csv']));
-        for stimuli = 1:2;
-            if stimuli == 1;
-                color = blue;
-            elseif stimuli == 2;
-                color = red;
+p.parse(varargin{:});
+
+
+%% first compute method: window
+% For this calculation of the PIPR, we are determining the sustained
+% amplitude for each subject, which refers to pupil diameter during a period of time after light
+% offset. the pipr then is the sustained amplitude of blue minus the sustained amplitude of the red
+% this approach more closely follows what is seen in the rest of the
+% literature, although the exact timing of the windows varies (can
+% generally either be an instaneous diameter at a certain moment, or the
+% average diameter over a period of time)
+
+if strcmp(p.Results.computeMethod, 'window')
+    
+    % First to calculate the sustained amplitudes
+    
+    % determine the sustained window
+    sustainedOnsetTime = p.Results.timeOn; 
+    sustainedOffsetTime = p.Results.timeOff; % to the end of an individual trial
+    % we are sampling pupil diameter every 20 ms, so this time window will
+    % include the following indices
+    sustainedWindow = (sustainedOnsetTime/0.02):(sustainedOffsetTime/0.02);
+    
+    % For each subject for each trial of red and blue light, determine the
+    % average sustained amplitude. Then determine the average sustained
+    % amplitude for each subject across all red or all blue trials
+    for session = 1:2;
+        sustainedAmplitudes{session} = [];
+        pipr{session} = [];
+        netPipr{session} = [];
+    end
+    
+    for session = 1:2;
+        for ss = 1:size(subjects{session}{1},1);
+            subject = subjects{session}{1}(ss,:);
+            numberSessions = dir(fullfile(dropboxAnalysisDir, 'PIPRMaxPulse_PulsePIPR', subject));
+            numberSessions =length(numberSessions(~ismember({numberSessions.name},{'.','..', '.DS_Store'})));
+            
+            date = subjects{session}{2}(ss,:);
+            
+            blue = importdata(fullfile(dropboxAnalysisDir, 'PIPRMaxPulse_PulsePIPR', subject, date, [subject, '_PupilPulseData_PIPRBlue_TimeSeries.csv']));
+            red = importdata(fullfile(dropboxAnalysisDir, 'PIPRMaxPulse_PulsePIPR', subject, date, [subject, '_PupilPulseData_PIPRRed_TimeSeries.csv']));
+            for stimuli = 1:2;
+                if stimuli == 1;
+                    color = blue;
+                elseif stimuli == 2;
+                    color = red;
+                end
+                sustainedStimulusCombined = [];
+                for trial = 1:size(color,2);
+                    sustainedStimulusCombined(trial) = nanmean(color(round(sustainedWindow),trial));
+                end
+                sustainedAmplitudes{session}(ss,stimuli) = nanmean(sustainedStimulusCombined);
             end
-            for trial = 1:size(color,2);
-                sustainedStimulusCombined(trial) = nanmean(color(sustainedWindow,trial));
-            end
-            sustainedAmplitudes{session}(ss,stimuli) = nanmean(sustainedStimulusCombined);
+        end
+    end
+    
+    % Now to calculate the actual PIPR values
+    for session = 1:2;
+        for ss = 1:size(subjects{session}{1},1);
+            pipr{session}(ss) = 0 - sustainedAmplitudes{session}(ss,1)*100;
+            netPipr{session}(ss) = ((0 - sustainedAmplitudes{session}(ss,1)) - (0 - sustainedAmplitudes{session}(ss,2)))*100;
         end
     end
 end
 
-%% Now to calculate the actual PIPR values
-for session = 1:2;
-    for ss = 1:size(subjects{session}{1},1);
-        pipr{session}(ss) = 0 - sustainedAmplitudes{session}(ss,1)*100;
-        netPipr{session}(ss) = ((0 - sustainedAmplitudes{session}(ss,1)) - (0 - sustainedAmplitudes{session}(ss,2)))*100;
+%% Second compute method: total amplitude
+% compute PIPR by determining amplitude based on the total time series. we
+% get a group average fit to both blue and red stimulus. for each subject,
+% determine the beta coefficient for the linear regression of this group
+% average to each individual subject; this will be our amplitude component.
+% determine the PIPR then by subtracting the blue amplitude from the red
+% amplitude
+
+if strcmp(p.Results.computeMethod, 'totalAmplitude')
+    for session = 1:2 % loop over session
+        for ss = 1:size(subjects{session}{1},1);
+            netPipr{session}(ss) = amplitudes{session}(ss, 3) - amplitudes{session}(ss, 4);
+        end
     end
 end
+
+% the pipr results variable doesn't make much sense in this context, it's
+% just the red and blue amplitude results already found in the amplitude
+% variable
+if strcmp(p.Results.computeMethod, 'totalAmplitude')
+    for session = 1:2 % loop over session
+        for ss = 1:size(subjects{session}{1},1);
+            pipr{session}(ss) = amplitudes{session}(ss, 3);
+        end
+    end
+end
+
 %% Plot these results to show how the different values of PIPR relate to each other
-subDir = 'pupilPIPRAnalysis/IAMP/calculatePIPR'
-
-% Plot correlation of this PIPR with IAMP PIPR
-for session = 1:2;
-    outDir = fullfile(dropboxAnalysisDir,subDir, num2str(session));
-    if ~exist(outDir, 'dir')
-        mkdir(outDir);
-    end
+if strcmp(p.Results.plot, 'yes')
+    subDir = 'pupilPIPRAnalysis/IAMP/calculatePIPR'
     
-    plotFig = figure;
-    plot(pipr{session}, ((amplitudes{session}(:,3)*100)-(amplitudes{session}(:,4)*100)), 'o');
-    xlabel('PIPR (Baseline - Sustained, %)')
-    ylabel('PIPR (IAMP, %)')
-    
-    saveas(plotFig, fullfile(outDir, ['piprxIAMPPipr_', num2str(sustainedOnsetTime), '.png']), 'png');
-    close(plotFig);
-    
-    
-    % Plot correlation of PIPR and net PIPR
-    plotFig = figure;
-    plot(pipr{session}, netPipr{session}, 'o');
-    xlabel('PIPR (Baseline - Sustained, %)')
-    ylabel('Net PIPR (Blue PIPR - Red PIPR, %)')
-    saveas(plotFig, fullfile(outDir, ['piprxNetPipr_', num2str(sustainedOnsetTime), '.png']), 'png');
-    close(plotFig);
-    
-    % Plot correlation of net PIPR and IAMP PIPR
-    plotFig = figure;
-    plot(netPipr{session}, ((amplitudes{session}(:,3)*100)-(amplitudes{session}(:,4)*100)), 'o');
-    xlabel('Net PIPR (Blue PIPR - Red PIPR, %)')
-    ylabel('PIPR (IAMP, %)')
-    saveas(plotFig, fullfile(outDir, ['netPiprxIAMPPipr_', num2str(sustainedOnsetTime), '.png']), 'png');
-    close(plotFig);
-    
-    % Plot correlation of PIPR and Melanopsin-directed silent substitution
-    plotFig = figure;
-    plot(pipr{session}, amplitudes{session}(:,2)*100, 'o');
-    xlabel('PIPR (Baseline - Sustained, %)')
-    ylabel('Melanopsin Silent Substitution Amplitude (%)')
-    saveas(plotFig, fullfile(outDir, ['piprxMel_', num2str(sustainedOnsetTime), '.png']), 'png');
-    close(plotFig);
-    
-    % Plot correlation of Net PIPR and Melanopsin-directed silent substitution
-    plotFig = figure;
-    plot(netPipr{session}, amplitudes{session}(:,2)*100, 'o');
-    xlabel('Net PIPR (Blue PIPR - Red PIPR, %)')
-    ylabel('Melanopsin Silent Substitution Amplitude (%)')
-    saveas(plotFig, fullfile(outDir, ['netPiprxMel_', num2str(sustainedOnsetTime), '.png']), 'png');
-    close(plotFig);
-    
-    % plot correlation of 6s net PIPR with mel amplitude
-    sixSecondPIPR = [];
-    for ss = 1:size(subjects{session}{1},1)
+    % Plot correlation of this PIPR with IAMP PIPR
+    for session = 1:2;
+        outDir = fullfile(dropboxAnalysisDir,subDir, num2str(session));
+        if ~exist(outDir, 'dir')
+            mkdir(outDir);
+        end
         
-        sixSecondPIPR(ss) = -(averageBlueCombined{session}(ss,500) - averageRedCombined{session}(ss,500));
-    end
-    plotFig = figure;
-    plot(amplitudes{session}(:,2)*100, sixSecondPIPR, 'o')
-    xlabel('Melanopsin Silent Substitution Amplitude (%)')
-    ylabel('Net PIPR (dR6 - dB6)')
-    saveas(plotFig, fullfile(outDir, ['netPipr6xMel_', '.png']), 'png');
-    saveas(plotFig, fullfile(outDir, ['netPipr6xMel_', '.pdf']), 'pdf');
-    
-    
-    close(plotFig);
-    prettyScatterplots(amplitudes{session}(:,2)*100, sixSecondPIPR'*100, sixSecondPIPR*0, sixSecondPIPR*0, 'xLabel', 'Melanopsin Silent Substitution Amplitude (%)', 'yLabel', 'Net PIPR (dR6 - dB6)', 'close', 'on', 'significance', 'rho', 'save', fullfile(outDir, ['6sNetPIPRxMel.pdf']), 'saveType', 'pdf')
-
-    
-    
-end % end loop over sessions
+        plotFig = figure;
+        plot(pipr{session}, ((amplitudes{session}(:,3)*100)-(amplitudes{session}(:,4)*100)), 'o');
+        xlabel('PIPR (Baseline - Sustained, %)')
+        ylabel('PIPR (IAMP, %)')
+        
+        saveas(plotFig, fullfile(outDir, ['piprxIAMPPipr_', num2str(sustainedOnsetTime), '.png']), 'png');
+        close(plotFig);
+        
+        
+        % Plot correlation of PIPR and net PIPR
+        plotFig = figure;
+        plot(pipr{session}, netPipr{session}, 'o');
+        xlabel('PIPR (Baseline - Sustained, %)')
+        ylabel('Net PIPR (Blue PIPR - Red PIPR, %)')
+        saveas(plotFig, fullfile(outDir, ['piprxNetPipr_', num2str(sustainedOnsetTime), '.png']), 'png');
+        close(plotFig);
+        
+        % Plot correlation of net PIPR and IAMP PIPR
+        plotFig = figure;
+        plot(netPipr{session}, ((amplitudes{session}(:,3)*100)-(amplitudes{session}(:,4)*100)), 'o');
+        xlabel('Net PIPR (Blue PIPR - Red PIPR, %)')
+        ylabel('PIPR (IAMP, %)')
+        saveas(plotFig, fullfile(outDir, ['netPiprxIAMPPipr_', num2str(sustainedOnsetTime), '.png']), 'png');
+        close(plotFig);
+        
+        % Plot correlation of PIPR and Melanopsin-directed silent substitution
+        plotFig = figure;
+        plot(pipr{session}, amplitudes{session}(:,2)*100, 'o');
+        xlabel('PIPR (Baseline - Sustained, %)')
+        ylabel('Melanopsin Silent Substitution Amplitude (%)')
+        saveas(plotFig, fullfile(outDir, ['piprxMel_', num2str(sustainedOnsetTime), '.png']), 'png');
+        close(plotFig);
+        
+        % Plot correlation of Net PIPR and Melanopsin-directed silent substitution
+        plotFig = figure;
+        plot(netPipr{session}, amplitudes{session}(:,2)*100, 'o');
+        xlabel('Net PIPR (Blue PIPR - Red PIPR, %)')
+        ylabel('Melanopsin Silent Substitution Amplitude (%)')
+        saveas(plotFig, fullfile(outDir, ['netPiprxMel_', num2str(sustainedOnsetTime), '.png']), 'png');
+        close(plotFig);
+        
+        
+    end % end loop over sessions
+end
+%%
 
 end % end function
