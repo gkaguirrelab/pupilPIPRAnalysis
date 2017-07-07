90fabdcf1b52e511f0bdf7b33c25465c3a9715c2
harrison-mcadams <harrisonmcadams@hntvlan554.00308.wlan.wireless-pennnet.upenn.edu>
2017-07-07 09:59:26 -0400

    can take different results, not just the mel-specific response

--- code/bootstrapRho.m
+++ code/bootstrapRho.m
@@ -1,7 +1,14 @@
-function [ confidenceInterval, meanRho, rhoCombined ] = bootstrapRho(goodSubjects, amplitudes)
+function [ confidenceInterval, meanRho, rhoCombined ] = bootstrapRho(goodSubjects, firstSessionResult, secondSessionResult)
 
-% combine the melanopsin-specific response index for subjects who have completed both sessions
-[melNormedCombined] = pairResultAcrossSessions(goodSubjects, amplitudes{1}(:,6), amplitudes{2}(:,6));
+% if the firstSessionResult and the secondSessionResult are not of the same
+% length, then we assume they have to be paired using the function
+% pairResultAcrossSessions
+if length(firstSessionResult) ~= length(secondSessionResult)
+    [resultCombined] = pairResultAcrossSessions(goodSubjects, firstSessionResult, secondSessionResult);
+else
+    resultCombined{1} = firstSessionResult;
+    resultCombined{2} = secondSessionResult;
+end
 
 % do the bootstrapping
 nBootstraps = 10000;
@@ -11,12 +18,22 @@ for bb = 1:nBootstraps
     nSubjects = 25;
     randomSubjects = randsample(1:nSubjects, nSubjects, true);
     
-    rho = corr(melNormedCombined{1}(randomSubjects)', melNormedCombined{2}(randomSubjects)', 'type', 'Spearman');
+    rho = corr(resultCombined{1}(randomSubjects)', resultCombined{2}(randomSubjects)', 'type', 'Spearman');
     rhoCombined(bb) = rho;
 end
 
 % now determine the 95% confidence interval:
-SEM = std(rhoCombined);
-tScore = tinv([0.025 0.975], length(rhoCombined)-1);
-confidenceInterval = mean(rhoCombined) + tScore*SEM
-meanRho = mean(rhoCombined)
+% leaving this for legacy purposes, but isn't the ideal way to calculate
+% the 95% confidence interval for correlation data (data aren't normally
+% distributed and are instead bound between -1 and 1)
+%SEM = std(rhoCombined);
+%tScore = tinv([0.025 0.975], length(rhoCombined)-1);
+%confidenceInterval = mean(rhoCombined) + tScore*SEM
+%meanRho = mean(rhoCombined)
+
+
+% now determine the 95% confidence interval
+sortedRho = sort(rhoCombined);
+confidenceInterval(1) = sortedRho(501);
+confidenceInterval(2) = sortedRho(9501);
+meanRho = mean(rhoCombined);
