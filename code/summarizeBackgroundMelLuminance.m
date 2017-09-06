993856fce4ff1a7b2ba765e90cbb8b1155431ac0
harrison-mcadams <harrisonmcadams@huntsman-ve504-0825.apn.wlan.upenn.edu>
2017-09-06 18:08:32 -0400

    added new pilot validations and ND changes

--- code/summarizeBackgroundMelLuminance.m
+++ code/summarizeBackgroundMelLuminance.m
@@ -1,8 +1,8 @@
 function [] = summarizeBackgroundMelLuminance(goodSubjects, dropboxAnalysisDir)
 
-pilotSubjects = {'HERO_HMM', 'HERO_NoFocus', 'HERO_HMM', 'HERO_postMove', 'MELA_0003', 'MELA_0038', 'newCodeTest'};
-pilotDates = {'072817', '080217', '080417', '080817', '081517', '081617', '081717'};
-pilotWhichValidationList = {'pre', 'pre', 'combined', 'pre', 'pre', 'pre', 'pre'};
+pilotSubjects = {'HERO_HMM', 'HERO_NoFocus', 'HERO_HMM', 'HERO_postMove', 'MELA_0003', 'MELA_0038', 'newCodeTest', 'MELA_0084', 'HERO_newCal00_redo'};
+pilotDates = {'072817', '080217', '080417', '080817', '081517', '081617', '081717', '082117', '082517'};
+pilotWhichValidationList = {'pre', 'pre', 'combined', 'pre', 'pre', 'pre', 'pre','pre', 'pre'};
     
 combinedSubjects = horzcat(goodSubjects{3}.ID, pilotSubjects);
 combinedDates = horzcat(goodSubjects{3}.date, pilotDates);
@@ -39,9 +39,12 @@ line([xlims(1), xlims(2)], [215, 215], 'Color', 'r', 'LineStyle', '--')
 
 % also want to add to the plot dates in which we've changed the ND filter
 % and re-calibrated
-datesWhenWeChangedNDFilter = {'081617'};
+datesWhenWeChangedNDFilter = {'081617', '082517'};
 % 081617: changed ND filter from 0.2 to 0.1 because light intensity dropped
-% below our desired rang
+% below our desired range
+% 082517: changed setup, including different bulb (previously the bulb that
+% was in box A), eyepiece 1 (switched from eyepiece 2), with 0 ND filter
+
 
 
 
