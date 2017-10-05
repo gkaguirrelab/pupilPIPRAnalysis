7d889c8555293057efdf27915d24f035749443eb
harrison-mcadams <harrisonmcadams@huntsman-ve503-0687.apn.wlan.upenn.edu>
2017-10-05 11:30:15 -0400

    overhauled the function
    -made it more flexible: doesn't matter which session has more or less
    subjects
    -also plots the correlation
    -parses input

--- helpers/pairResultAcrossSessions.m
+++ helpers/pairResultAcrossSessions.m
@@ -1,12 +1,78 @@
-function [ pairedResult ]  = pairResultAcrossSessions(goodSubjects, sessionOneResult, sessionTwoResult)
+function [ pairedResult ]  = pairResultAcrossSessions(sessionOneSubjectList, sessionTwoSubjectList, sessionOneResult, sessionTwoResult, dropboxAnalysisDir, varargin)
 
-for ss = 1:length(goodSubjects{2}.ID) % loop over subjects that have completed both sessions
-    subject = goodSubjects{2}.ID(ss);
+dbstop if error
+
+%% Parse input
+p = inputParser; p.KeepUnmatched = true;
+
+
+p.addParameter('makePlot',true,@islogical);
+p.addParameter('significance','rho',@ischar);
+p.addParameter('subdir','',@ischar);
+p.addParameter('saveName','unnamed',@ischar);
+p.addParameter('sessionOneErrorBar','',@isnumeric);
+p.addParameter('sessionTwoErrorBar','',@isnumeric);
+p.addParameter('xLims',[0 10],@isnumeric);
+p.addParameter('yLims',[0 10],@isnumeric);
+
+p.parse(varargin{:});
+
+
+%% do the pairing
+pairedResult.sessionOne = [];
+pairedResult.sessionTwo = [];
+pairedResult.subjectKey = [];
+
+for ss = 1:length(sessionTwoSubjectList.ID) % loop over subjects that have completed both sessions
+    subject = sessionTwoSubjectList.ID(ss);
     
     secondSessionIndex = ss;
-    whichSubject = cellfun(@(x) strcmp(x, subject), goodSubjects{1}.ID);
-    [maxValue, firstSessionIndex] = max(whichSubject);
-    pairedResult.sessionOne(ss) = sessionOneResult(firstSessionIndex);
-    pairedResult.sessionTwo(ss) = sessionTwoResult(secondSessionIndex);
-   
+    whichSubject = cellfun(@(x) strcmp(x, subject), sessionOneSubjectList.ID);
+    if sum(whichSubject) ~= 0
+        [maxValue, firstSessionIndex] = max(whichSubject);
+        pairedResult.sessionOne = [pairedResult.sessionOne, sessionOneResult(firstSessionIndex)];
+        pairedResult.sessionTwo = [pairedResult.sessionTwo, sessionTwoResult(secondSessionIndex)];
+        pairedResult.subjectKey = [pairedResult.subjectKey, sessionTwoSubjectList.ID(ss)];
+    end
+    
 end
+
+
+% now do the pairing for the error bars if called for
+if isempty(p.Results.sessionOneErrorBar)
+else
+    pairedResult.sessionOneErrorBar = [];
+    pairedResult.sessionTwoErrorBar = [];
+    for ss = 1:length(sessionTwoSubjectList.ID) % loop over subjects that have completed both sessions
+        subject = sessionTwoSubjectList.ID(ss);
+        
+        secondSessionIndex = ss;
+        whichSubject = cellfun(@(x) strcmp(x, subject), sessionOneSubjectList.ID);
+        if sum(whichSubject) ~= 0
+            [maxValue, firstSessionIndex] = max(whichSubject);
+            pairedResult.sessionOneErrorBar = [pairedResult.sessionOneErrorBar, p.Results.sessionOneErrorBar(firstSessionIndex)];
+            pairedResult.sessionTwoErrorBar = [pairedResult.sessionTwoErrorBar, p.Results.sessionTwoErrorBar(secondSessionIndex)];
+        end
+        
+    end
+end
+
+%% do the plotting
+if p.Results.makePlot
+    plotFig = figure;
+    hold on
+    plot(-100:100, -100:100, '-.', 'Color', 'k')
+    if isempty(p.Results.sessionOneErrorBar)
+       prettyScatterplots(pairedResult.sessionOne, pairedResult.sessionTwo, 0*pairedResult.sessionOne, 0*pairedResult.sessionOne, 'xLim', p.Results.xLims, 'yLim', p.Results.yLims, 'significance', p.Results.significance)
+
+    else
+       prettyScatterplots(pairedResult.sessionOne, pairedResult.sessionTwo, pairedResult.sessionOneErrorBar, pairedResult.sessionTwoErrorBar, 'xLim', p.Results.xLims, 'yLim', p.Results.yLims, 'significance', p.Results.significance)
+
+    end
+    outDir = fullfile(dropboxAnalysisDir,'pupilPIPRAnalysis', p.Results.subdir, 'testRetest');
+    if ~exist(outDir, 'dir')
+        mkdir(outDir);
+    end
+    saveas(plotFig, fullfile(outDir, [p.Results.saveName, '.png']), 'png')
+end
+end % end function
