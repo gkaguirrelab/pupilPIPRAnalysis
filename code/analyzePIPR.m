74efcd64cb6523e6fee6ae774983cd85fc15dd32
harrison-mcadams <harrisonmcadams@huntsman-ve503-0687.apn.wlan.upenn.edu>
2017-10-18 18:37:19 -0400

    new code to analyze PIPR results
    new code that plots a bunch of comparisons to better understand the
    PIPR signal
    -compares different methods of computing the PIPR
    -compares PIPRs across sessions
    -compares PIPR to measures of the melanopsin response elicited through
    silent subsitution

--- code/analyzePIPR.m
+++ code/analyzePIPR.m
@@ -1,43 +1,118 @@
-function analyzePIPR(goodSubjects, amplitudes, averageBlueCombined, averageRedCombined, dropboxAnalysisDir)
+function analyzePIPR(goodSubjects, amplitudesPerSubject, dropboxAnalysisDir)
 
-% The purpose of this function is to determine which calculation of the
-% PIPR gives us the largest value. This function averages computes the net
-% PIPR at various time windows, and then plots the average PIPR across
-% subjects against each time window
+% Although actually computing the PIPR is trivial, making sense of the
+% result is challenging. This function creates various plots to attempt to
+% understand different aspects of the PIPR: how different measures of the
+% PIPR relate to each other, how varying light intensity affects the PIPR,
+% and how PIPR relates to our other measures of the melanopsin response.
 
-subDir = 'pupilPIPRAnalysis/IAMP/calculatePIPR';
-outDir = fullfile(dropboxAnalysisDir,subDir);
+
+%% there are different methods by which we can compute the PIPR response: how do they relate to each other?
+outDir = fullfile(dropboxAnalysisDir,'pupilPIPRAnalysis/PIPR', 'comparePIPRs');
 if ~exist(outDir, 'dir')
     mkdir(outDir);
 end
 
-% First, look at pipr computed at a single timepoint
-index = 0;
-for tt = 4:0.02:13.98 % loop over every time point from light offset to the end of the run
-    index = index + 1;
-    [pipr, netPipr]  = calculatePIPR(goodSubjects, amplitudes, dropboxAnalysisDir, 'computeMethod', 'window', 'timeOn', tt, 'timeOff', tt);
-    [ netPIPRCombined ] = combineResultAcrossSessions(goodSubjects, netPipr{1}, netPipr{2});
-    averagePIPR(index) = mean(netPIPRCombined);
-end
-plotFig1 = figure;
-plot(4:0.02:13.98, averagePIPR, 'o')
-xlabel('Timepoint when PIPR was Evaluated')
-ylabel('Average Net PIPR')
-saveas(plotFig1, fullfile(outDir, ['instantaneous.pdf']), 'pdf');
-close(plotFig1)
+% compute the PIPR by the window approach. Specifically, designate the
+% entire period following light-off as the sustained window, and calculate
+% the PIPR based on the average amplitude of sustained constriction during
+% this sustained window. Note of course that there are lots of ways we
+% could determine this window
+[piprWindow, netPIPRWindow] = calculatePIPR(goodSubjects, amplitudesPerSubject, dropboxAnalysisDir, 'computeMethod', 'window', 'timeOn', 4, 'timeOff', 14);
 
-% Second, look at pipr computed at a from a given timepoint until the end
-% of the trial
-index = 0;
-for tt = 4:0.02:13.98 % loop over every time point from light offset to the end of the run
-    index = index + 1;
-    [pipr, netPipr]  = calculatePIPR(goodSubjects, amplitudes, dropboxAnalysisDir, 'computeMethod', 'window', 'timeOn', tt, 'timeOff', 14);
-    [ netPIPRCombined ] = combineResultAcrossSessions(goodSubjects, netPipr{1}, netPipr{2});
-    averagePIPR(index) = mean(netPIPRCombined);
+% We also proposed to calculate the PIPR on the basis of our amplitude of
+% pupil constriction to blue and red stimuli obtained via IAMP modeling.
+% Specifically the PIPR then is the amplitude of constriction in response
+% to blue stimulation minus the amplitude of constriction in response to
+% red stimulation
+[piprTotalAmplitude, netPIPRTotalAmplitude] = calculatePIPR(goodSubjects, amplitudesPerSubject, dropboxAnalysisDir, 'computeMethod', 'totalAmplitude');
+
+% In our pre-registration for the third session, we specifically said we
+% would normalize this measure of the PIPR for overall pupil response
+% amplitude (here the sum of amplitude of constriction to blue and red
+% stimulus) So the netPIPRTotalAmplitudeNormed is (Blue-Red)/(Blue+Red)
+[piprTotalAmplitudeNormed, netPIPRTotalAmplitudeNormed] = calculatePIPR(goodSubjects, amplitudesPerSubject, dropboxAnalysisDir, 'computeMethod', 'totalAmplitudeNormed');
+
+% now look at some plots to look at the relationships of these different
+% measures. All of these comparisons are going to be exlusively with the
+% net PIPR (ie blue - red) because we believe we need to red response to
+% assess any residual cone contribution
+for session = 1:3
+    
+    % window vs. total amplitude
+    plotFig = figure;
+    prettyScatterplots(netPIPRWindow{session}, netPIPRTotalAmplitude{session}*100, netPIPRWindow{session}*0, netPIPRWindow{session}*0, 'xLim', [-7 12], 'yLim', [-5 25], 'significance', 'rho')
+    xlabel('Net PIPR from Window Method (% Change)')
+    ylabel('Net PIPR from Total Amplitude Method (% Change)')
+    saveas(plotFig, fullfile(outDir, 'windowXTotalAmplitude.png'), 'png')
+    
+    % window vs. total amplitude normed
+    plotFig = figure;
+    prettyScatterplots(netPIPRWindow{session}, netPIPRTotalAmplitudeNormed{session}*100, netPIPRWindow{session}*0, netPIPRWindow{session}*0, 'xLim', [-7 12], 'yLim', [-5 27], 'significance', 'rho')
+    xlabel('Net PIPR from Window Method (% Change)')
+    ylabel('Net PIPR from Total Amplitude Method Normed (%)')
+    saveas(plotFig, fullfile(outDir, 'windowXTotalAmplitudeNormed.png'), 'png')
+    
+    % total amplitude vs. total amplitude normed
+    plotFig = figure;
+    prettyScatterplots(netPIPRTotalAmplitude{session}*100, netPIPRTotalAmplitudeNormed{session}*100, netPIPRWindow{session}*0, netPIPRWindow{session}*0, 'xLim', [-7 12], 'yLim', [-10 25], 'significance', 'rho')
+    xlabel('Net PIPR from TotalAmplitude (% Change)')
+    ylabel('Net PIPR from Total Amplitude Method Normed (%)')
+    saveas(plotFig, fullfile(outDir, 'totalAmplitudeXTotalAmplitudeNormed.png'), 'png')
+    
 end
-plotFig2 = figure;
-plot(4:0.02:13.98, averagePIPR, 'o')
-xlabel('Timepoint for beginning of window when PIPR was Evaluated')
-ylabel('Average Net PIPR')
-saveas(plotFig2, fullfile(outDir, ['window.pdf']), 'pdf');
-close(plotFig2)
+
+close all
+
+%% How Session 3 relates to session 1/2
+outDir = fullfile(dropboxAnalysisDir,'pupilPIPRAnalysis/PIPR', 'compareSessions');
+if ~exist(outDir, 'dir')
+    mkdir(outDir);
+end
+
+
+PIPRs = {'netPIPRWindow', 'netPIPRTotalAmplitude', 'netPIPRTotalAmplitudeNormed'};
+for pp = 1:length(PIPRs)
+    plotFig = figure;
+    hold on
+    result = eval(PIPRs{pp});
+    bplot(result{1}, 1, 'color', 'k');
+    bplot(result{2}, 2, 'color', 'k');
+    bplot(result{3}, 3, 'color', 'k');
+    xlabel('Session')
+    ylabel(PIPRs{pp})
+    saveas(plotFig, fullfile(outDir, [PIPRs{pp}, '_boxPlot.png']), 'png')
+    
+    plotFig = figure;
+    data = horzcat({result{1}', result{2}', result{3}'});
+    plotSpread(data, 'distributionMarkers', 'o', 'xNames', {'Session 1', 'Session 2', 'Session 3'})
+    ylabel(PIPRs{pp})
+    saveas(plotFig, fullfile(outDir, [PIPRs{pp}, '_spread.png']), 'png')
+end
+
+close all
+
+%% How do different measurs of the PIPR relate to measures of the melanopsin response elicited through silent substitution?
+
+
+SSMethods = {'Mel', 'MeltoLMS'};
+PIPRs = {'netPIPRWindow', 'netPIPRTotalAmplitude', 'netPIPRTotalAmplitudeNormed'};
+for session = 1:3
+    outDir = fullfile(dropboxAnalysisDir,'pupilPIPRAnalysis/PIPR', 'compareStimulationMethod', num2str(session));
+    if ~exist(outDir, 'dir')
+        mkdir(outDir);
+    end
+    for xx = 1:length(PIPRs)
+        for yy = 1:length(SSMethods)
+            PIPRResult = eval(PIPRs{xx});
+            
+            plotFig = figure;
+            prettyScatterplots(PIPRResult{session}, amplitudesPerSubject{session}.(SSMethods{yy}), 0*PIPRResult{session}, PIPRResult{session}*0, 'xLabel', PIPRs{xx}, 'yLabel', SSMethods{yy}, 'significance', 'rho')
+            saveas(plotFig, fullfile(outDir, [PIPRs{xx}, 'x', SSMethods{yy}, '.png']), 'png')
+        end
+    end
+end
+close all
+
+end % end function
+
