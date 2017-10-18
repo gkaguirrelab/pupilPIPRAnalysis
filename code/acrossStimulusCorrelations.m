c5ff4a2fa7c0a4ad935a2d1259d67f919a441591
harrison-mcadams <harrisonmcadams@huntsman-ve503-0687.apn.wlan.upenn.edu>
2017-10-18 18:37:30 -0400

    added new comparisons

--- code/acrossStimulusCorrelations.m
+++ code/acrossStimulusCorrelations.m
@@ -1,8 +1,10 @@
-function acrossStimulusCorrelations(amplitudesPerSubject, dropboxAnalysisDir)
+function acrossStimulusCorrelations(amplitudesPerSubject, totalResponseArea, dropboxAnalysisDir)
 
+
+%% first the IAMP comparisons
 % set up where to save our plots
 subDir = 'pupilPIPRAnalysis/IAMP/acrossStimulusCorrelations';
-comparisonsX = {'LMS', 'Blue', 'SilentSubstitionAverage', 'BluetoRed'};
+comparisonsX = {'LMS', 'Blue', 'SilentSubstitutionAverage', 'BluetoRed'};
 comparisonsY = {'Mel', 'Red', 'PIPRAverage', 'MeltoLMS'};
 
 for session = 1:length(amplitudesPerSubject)
@@ -18,6 +20,29 @@ for session = 1:length(amplitudesPerSubject)
         close(plotFig)
     end
 end
+
+%% now the TPUP comparisons
+% set up where to save our plots
+subDir = 'pupilPIPRAnalysis/TPUP/acrossStimulusCorrelations';
+comparisonsX = {'LMS', 'Blue'};
+comparisonsY = {'Mel', 'Red'};
+mins = [-220 -330];
+maxs = [0 0];
+
+for session = 1:length(amplitudesPerSubject)
+    outDir = fullfile(dropboxAnalysisDir, subDir, num2str(session));
+    if ~exist(outDir, 'dir')
+        mkdir(outDir);
+    end
+    
+    for comparison = 1:length(comparisonsX)
+        
+        plotFig = figure;
+        prettyScatterplots(totalResponseArea{session}.(comparisonsX{comparison}), totalResponseArea{session}.(comparisonsY{comparison}), totalResponseArea{session}.([comparisonsX{comparison}]).*0, totalResponseArea{session}.([comparisonsY{comparison}]).*0, 'xLim', [mins(comparison) maxs(comparison)], 'yLim', [mins(comparison) maxs(comparison)], 'unity', 'on', 'plotOption', 'square', 'xLabel', [comparisonsX{comparison}, ' Amplitude (%)'], 'yLabel', [comparisonsY{comparison}, ' Amplitude (%)'], 'lineOfBestFit', 'on', 'significance', 'spearman', 'save', fullfile(dropboxAnalysisDir, subDir, num2str(session), ['totalResponseArea_', comparisonsX{comparison}, 'x' comparisonsY{comparison}, '.png']), 'saveType', 'png')
+        close(plotFig)
+    end
+end
+
 
 
 
