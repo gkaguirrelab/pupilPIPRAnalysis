cc89684aa8b168f2242eb4dd23a1cc5042af38c5
harrison-mcadams <harrisonmcadams@huntsman-ve500-0669.apn.wlan.upenn.edu>
2017-10-19 17:03:27 -0400

    added new comparison
    -compared average response for SS and PIPR stimuli using the TPUP model

--- code/acrossStimulusCorrelations.m
+++ code/acrossStimulusCorrelations.m
@@ -43,8 +43,18 @@ for session = 1:length(amplitudesPerSubject)
     end
 end
 
-
-
+% now to compare silent substitution and PIPR averages
+for session = 1:length(amplitudesPerSubject)
+    outDir = fullfile(dropboxAnalysisDir, subDir, num2str(session));
+    if ~exist(outDir, 'dir')
+        mkdir(outDir);
+    end
+    plotFig = figure;
+    prettyScatterplots((totalResponseArea{session}.Mel+totalResponseArea{session}.LMS)/2, (totalResponseArea{session}.Blue+totalResponseArea{session}.Red)/2, 0*(totalResponseArea{session}.Mel+totalResponseArea{session}.LMS)/2, 0*(totalResponseArea{session}.Mel+totalResponseArea{session}.LMS)/2, 'xLim', [-350 0], 'yLim', [-350 0], 'unity', 'on', 'significance', 'rho', 'plotOption', 'square', 'lineOfBestFit', 'on')
+    xlabel('Silent Substitution Average Response Area')
+    ylabel('PIPR Average Response Area')
+    saveas(plotFig, fullfile(outDir, 'totalResponseArea_SSxPIPR.png'), 'png')
+end
 
 close all
 
