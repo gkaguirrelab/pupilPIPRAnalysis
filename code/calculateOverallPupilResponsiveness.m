bed1256adc60aec6ec621b9117c37823f21c3f9e
harrison-mcadams <harrisonmcadams@huntsman-ve506-0099.apn.wlan.upenn.edu>
2017-10-24 15:54:43 -0400

    added input parser to control plotting behavior

--- code/calculateOverallPupilResponsiveness.m
+++ code/calculateOverallPupilResponsiveness.m
@@ -1,4 +1,12 @@
-function [ overallPupilResponsiveness ] = calculateOverallPupilResponsiveness(goodSubjects, totalResponseArea, dropboxAnalysisDir)
+function [ overallPupilResponsiveness ] = calculateOverallPupilResponsiveness(goodSubjects, totalResponseArea, dropboxAnalysisDir, varargin)
+
+p = inputParser; p.KeepUnmatched = true;
+
+
+p.addParameter('makePlots',false,@islogical);
+
+
+p.parse(varargin{:});
 
 for session = 1:length(totalResponseArea)
     for ss = 1:length(totalResponseArea{session}.LMS)
@@ -6,5 +14,7 @@ for session = 1:length(totalResponseArea)
     end
 end
 
-[ pairedOverallResponsiveness ]  = pairResultAcrossSessions(goodSubjects{1}.ID, goodSubjects{2}.ID, overallPupilResponsiveness{1}, overallPupilResponsiveness{2}, dropboxAnalysisDir, 'xLabel', 'Session 1 Average Responsiveness', 'yLabel', 'Session 2 Average Responsiveness', 'significance', 'rho', 'xLims', [-225 0], 'yLims', [-225 0], 'subdir', 'TPUP', 'saveName', 'overallPupilResponsiveness_1x2');
+if p.Results.makePlots
+    [ pairedOverallResponsiveness ]  = pairResultAcrossSessions(goodSubjects{1}.ID, goodSubjects{2}.ID, overallPupilResponsiveness{1}, overallPupilResponsiveness{2}, dropboxAnalysisDir, 'xLabel', 'Session 1 Average Responsiveness', 'yLabel', 'Session 2 Average Responsiveness', 'significance', 'rho', 'xLims', [-225 0], 'yLims', [-225 0], 'subdir', 'TPUP', 'saveName', 'overallPupilResponsiveness_1x2');
+end
 end % end function
