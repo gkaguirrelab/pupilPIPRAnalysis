71d4f60249b94116d1ce9235ad04066b582315ad
harrison-mcadams <harrisonmcadams@huntsman-ve505-0229.apn.wlan.upenn.edu>
2017-11-17 18:13:04 -0500

    switched effect size
    -i had previously mis-remembered that the effect size described in that
    PD paper was about a 10% reduction, when really we wanted to describe
    it as a 33% reduction

--- code/simulationForPowerAnalysis.m
+++ code/simulationForPowerAnalysis.m
@@ -1,7 +1,7 @@
 function simulationForPowerAnalysis(goodSubjects, TPUPParameters)
 % This function runs simulations in order to determine how much power we have to detect
 % group differences. The simulations are based on our observed data. To
-% make our experimental/sick group, we are applying a 10% reduction to the
+% make our experimental/sick group, we are applying a 33% reduction to the
 % persistent amplitude component of the Mel response, based on the work by
 % Joyce et al (https://www.biorxiv.org/content/early/2017/07/28/169946).
 % For now, this function determines how power varies as a function of the
@@ -21,7 +21,7 @@ for stimulus = 1:length(stimuli)
 end
 %% set important variablers
 alpha = 0.01;
-effectSize = 0.1; % the idea we're running with for now is that the true effect of disease is a 10% reduction in the magnitude of the persistent component
+effectSize = 1/3; % the idea we're running with for now is that the true effect of disease is a 33% reduction in the magnitude of the persistent component
 
 %% as we vary the number of subjects, compute the power
 for ii = 1:50
@@ -46,23 +46,23 @@ for ss = 1:nSimulations
     experimentalSubjectIndices = randsample(1:length(combinedTPUPParameters.LMS.transientAmplitude), nSubjects, true);
     
     % gather mel/lms response ratios for our control group
-    controlMeltoLMS = (combinedTPUPParameters.Mel.transientAmplitude(controlSubjectIndices) + combinedTPUPParameters.Mel.sustainedAmplitude(controlSubjectIndices) + combinedTPUPParameters.Mel.persistentAmplitude(controlSubjectIndices))./(combinedTPUPParameters.LMS.transientAmplitude(controlSubjectIndices) + combinedTPUPParameters.LMS.sustainedAmplitude(controlSubjectIndices) + combinedTPUPParameters.LMS.persistentAmplitude(controlSubjectIndices));
+    %controlMeltoLMS = (combinedTPUPParameters.Mel.transientAmplitude(controlSubjectIndices) + combinedTPUPParameters.Mel.sustainedAmplitude(controlSubjectIndices) + combinedTPUPParameters.Mel.persistentAmplitude(controlSubjectIndices))./(combinedTPUPParameters.LMS.transientAmplitude(controlSubjectIndices) + combinedTPUPParameters.LMS.sustainedAmplitude(controlSubjectIndices) + combinedTPUPParameters.LMS.persistentAmplitude(controlSubjectIndices));
     
     % gather mel/lms response ratios for our experimental group
     % note here is how we're applying our effect size, from https://www.biorxiv.org/content/biorxiv/early/2017/07/28/169946.full.pdf
     % we're saying the consequence of disease is a 10% reduction in the
     % persistent amplitude for mel
-    experimentalMeltoLMS = (combinedTPUPParameters.Mel.transientAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.Mel.sustainedAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.Mel.persistentAmplitude(experimentalSubjectIndices)*(1-effectSize))./(combinedTPUPParameters.LMS.transientAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.LMS.sustainedAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.LMS.persistentAmplitude(experimentalSubjectIndices));
+    %experimentalMeltoLMS = (combinedTPUPParameters.Mel.transientAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.Mel.sustainedAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.Mel.persistentAmplitude(experimentalSubjectIndices)*(1-effectSize))./(combinedTPUPParameters.LMS.transientAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.LMS.sustainedAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.LMS.persistentAmplitude(experimentalSubjectIndices));
     % evaluate the significance of this group difference
     
-    [h, p] = ttest2(controlMeltoLMS, experimentalMeltoLMS, 'Tail', 'right');
+    %[h, p] = ttest2(controlMeltoLMS, experimentalMeltoLMS, 'Tail', 'right');
     
     % below is a bit of code if instead of looking at the mel/lms response
     % ratio, just looking if the total response area for mel differs
     % between these two groups
-    %controlMel = abs((combinedTPUPParameters.Mel.transientAmplitude(controlSubjectIndices) + combinedTPUPParameters.Mel.sustainedAmplitude(controlSubjectIndices) + combinedTPUPParameters.Mel.persistentAmplitude(controlSubjectIndices)));
-    %experimentalMel = abs((combinedTPUPParameters.Mel.transientAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.Mel.sustainedAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.Mel.persistentAmplitude(experimentalSubjectIndices)*(1-effectSize)));
-    %[h, p] = ttest2(controlMel, experimentalMel, 'Tail', 'right');
+    controlMel = abs((combinedTPUPParameters.Mel.transientAmplitude(controlSubjectIndices) + combinedTPUPParameters.Mel.sustainedAmplitude(controlSubjectIndices) + combinedTPUPParameters.Mel.persistentAmplitude(controlSubjectIndices)));
+    experimentalMel = abs((combinedTPUPParameters.Mel.transientAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.Mel.sustainedAmplitude(experimentalSubjectIndices) + combinedTPUPParameters.Mel.persistentAmplitude(experimentalSubjectIndices)*(1-effectSize)));
+    [h, p] = ttest2(controlMel, experimentalMel, 'Tail', 'right');
     
     % stash the p value
     pAccumulator = [pAccumulator, p];
