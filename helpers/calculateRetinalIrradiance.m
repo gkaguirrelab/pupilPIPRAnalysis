688fd915284284006998aca167e19bedf092c03b
harrison-mcadams <harrisonmcadams@huntsman-ve504-0449.apn.wlan.upenn.edu>
2018-01-09 16:01:21 -0500

    new function to calculateRetinalIrradiances
    based off code Manuel sent over to me, this function reads in a
    validation file, and from that files extracts the retinal irradiance of
    the stimulus

---
+++ helpers/calculateRetinalIrradiance.m
@@ -0,0 +1,39 @@
+function [ retinalIrradiance ] = calculateRetinalIrradiance(validationFile)
+
+% The purpose of this function is to load up an individual validation file
+% and extract the retinal irradiance of the stimulus. The meat of this code
+% was written by Manuel Spitschan, I (HMM) amd just packaging it up for my
+% purposes
+
+%% Load the file
+tmp = load(validationFile);
+
+% Extract the wavelength sampling spec and turn into a wavelength vector
+S = tmp.cals{end}.modulationBGMeas.meas.pr650.S;
+wls = SToWls(S);
+
+% Extract the background spd and the modulation spd
+bgSpd = tmp.cals{end}.modulationBGMeas.meas.pr650.spectrum;
+modSpd = tmp.cals{end}.modulationMaxMeas.meas.pr650.spectrum;
+
+% Plot them
+figure;
+plot(wls, bgSpd, '-k'); hold on;
+plot(wls, modSpd, '-b', 'LineWidth', 2);
+pbaspect([1 1 1]); set(gca, 'TickDir', 'out');
+xlabel('Wavelength'); ylabel('Radiance [mW/sr/cm-1/nm]');
+
+%% (1) Calculate the properties of the light
+pupilDiameterMm = 6;
+out1 = CalculateLightProperties(S, modSpd, pupilDiameterMm);
+
+
+%% (2) Now, filter the stimulus by the pre-receptoral filtering and do the calculations again
+observerAgeInYears =  tmp.cals{end}.describe.OBSERVER_AGE;
+fieldSizeDeg = tmp.cals{end}.describe.cache.data(observerAgeInYears).describe.params.fieldSizeDegrees;
+lensTransmit = LensTransmittance(S, 'Human', 'CIE', observerAgeInYears, pupilDiameterMm);
+macTransmit = MacularTransmittance(S, 'Human', 'CIE', fieldSizeDeg);
+modSpdFiltered = modSpd .* lensTransmit' .* macTransmit';
+
+retinalIrradiance = CalculateLightProperties(S, modSpdFiltered, pupilDiameterMm);
+end % end function
